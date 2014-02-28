694ee16d2ea9c3f2da69e041560f72b89b2ed169
Valera19900 <kaktuz34@gmail.com>
2014-02-28 20:03:51 +0200

    upd

--- js/common.js
+++ js/common.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
 
+	$(document).click(function() {
+        $(".js-popup").hide();
+    });
+        $(".js-popup").on("click",function(event){
+        		event.stopPropagation();
+        });
+        $(".js-popup-close").on("click",function(event){
+        		$(".js-popup").hide();
+        		event.stopPropagation();
+        });
+  
+
+			$('.fancybox').fancybox();
+
+		
 });
