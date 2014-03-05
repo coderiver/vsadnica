9990ad95d1ab98a85c59562e7dc64506c3fc2194
Valera19900 <kaktuz34@gmail.com>
2014-03-05 15:36:29 +0200

    fixed

--- js/common.js
+++ js/common.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
 	$(document).click(function() {
         $(".js-popup-field").hide();
+        $(".js-btn-sbm").parent().addClass("is-active");
     });
         $(".js-popup").on("click",function(event){
         		event.stopPropagation();
@@ -9,13 +10,20 @@ $(document).ready(function() {
         $(".js-popup-close").on("click",function(event){
         		$(".js-popup-field").hide();
         		event.stopPropagation();
-        });
-  
-        
-		$('.fancybox').fancybox();
-
+        });        
         $('.js-btn').click(function() {
             $(".js-popup-field").show();
             return false;
         });
+
+    ymaps.ready(function () {
+        var myMap = new ymaps.Map('YMapsID', {
+            center: [56.0009, 37.7220],
+            zoom: 13,
+            // Обратите внимание, что в API 2.1 по умолчанию карта создается с элементами управления.
+            // Если вам не нужно их добавлять на карту, в ее параметрах передайте пустой массив в поле controls.
+            controls: []
+        });
+    });
 });
+    $('.fancybox').fancybox();
